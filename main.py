7ad285310d5a4a5dcd722d003f0fe70906cca285
Henrique <soratanobaka666@gmail.com>
2026-05-13 13:37:50 -0300

    Revert

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from apps.auth.router import router as auth_router
 from apps.filmes.router import router as filmes_router
 from apps.perfil.router import router as perfil_router
 from apps.avaliacoes.router import router as avaliacoes_router
-from apps.meus_filmes.router import router as meus_filmes_router
-from apps.dashboard.router import router as dashboard_router
 
 app = FastAPI(title="Amnesia – Cinema")
 
@@ -33,5 +31,3 @@ app.include_router(auth_router)
 app.include_router(filmes_router)
 app.include_router(perfil_router)
 app.include_router(avaliacoes_router)
-app.include_router(meus_filmes_router)
-app.include_router(dashboard_router)
