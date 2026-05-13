2107fb6e9608a89ad00a554f38dcbacf6ee47ec6
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2026-05-13 20:09:52 -0300

    feat: atualiza token a cada intereção do usuario se o token estiver ativo

--- core/dependencies.py
+++ core/dependencies.py
@@ -1,5 +1,5 @@
 from fastapi import Request
-from core.security import decode_token
+from core.security import decode_token, refresh_access_token
 
 
 def get_current_user(request: Request) -> dict | None:
@@ -10,6 +10,10 @@ def get_current_user(request: Request) -> dict | None:
     payload = decode_token(token)
     if not payload:
         return None
+
+    new_token = refresh_access_token(token)
+    if new_token:
+        request.session["token"] = new_token
 
     return {
         "id":           payload.get("sub"),
